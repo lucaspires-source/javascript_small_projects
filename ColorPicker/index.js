f04d723b834489eb127b5684d52fa22a774b6808
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-01-20 17:44:06 -0300

    finished color picker

--- ColorPicker/index.js
+++ ColorPicker/index.js
@@ -1,1 +1,17 @@
-console.log("o pai ta on")
+const colors = ["green","red","#f15025","rgba(133,122,200"]
+
+const btn = document.getElementById("btn")
+const color = document.querySelector('.color')
+
+
+btn.addEventListener('click', () => {
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+
+})
+ const getRandomNumber =() =>{
+    return parseInt(Math.random()*colors.length)
+
+ }
