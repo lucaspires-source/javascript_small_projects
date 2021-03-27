13840e77684bea0072ef79b673ac5b92d9ac6bd5
@lucaspires-source <pires.lucas94@gmail.com>
2021-03-27 16:53:57 -0300

    made de counter project

--- Counter/index.js
+++ Counter/index.js
@@ -1,1 +1,28 @@
-console.log("o pai ta on")
+
+//Set initial count
+let count = 0;
+
+//select value and buttond
+
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach((btn) =>{
+    btn.addEventListener('click', (e) =>{
+        e.preventDefault()
+        const styles = e.currentTarget.classList
+        if(styles.contains ('decrease')){
+            count--
+            value.textContent=count
+        }
+        if(styles.contains ('increase')){
+            count++
+            value.textContent=count
+        }
+        if(styles.contains ('reset')){
+            count = 0
+            value.textContent=count
+        }
+    })
+})
+
